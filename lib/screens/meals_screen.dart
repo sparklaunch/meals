6073679e32475833a044c2e2c55bc06b9b0fee14
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-23 16:47:24 +0900

    Update

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   final String title;
@@ -9,7 +10,7 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (context, index) => Text(meals[index].title),
+      itemBuilder: (context, index) => MealItem(meal: meals[index]),
     );
     if (meals.isEmpty) {
       content = Center(
