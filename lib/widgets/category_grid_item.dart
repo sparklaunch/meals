ba170844c8c839b633b1922a6683f52e93b20f34
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-23 14:39:23 +0900

    Update

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  final Category category;
+  const CategoryGridItem({super.key, required this.category});
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
