6b0a638c94128c82d83a512aa5e63485a7a1feb9
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-27 14:59:13 +0900

    Update tabs_screen.dart

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -21,7 +21,7 @@ class _TabsScreenState extends State<TabsScreen> {
     var activePageTitle = "Categories";
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: const [],
+        meals: _favoriteMeals,
         onToggleFavorite: _toggleMealFavoriteStatus,
       );
       activePageTitle = "Favorites";
@@ -51,12 +51,27 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
     if (isExisting) {
-      _favoriteMeals.remove(meal);
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage("Meal is no longer a favorite.");
     } else {
-      _favoriteMeals.add(meal);
+      setState(() {
+        _favoriteMeals.add(meal);
+      });
+      _showInfoMessage("Marked as a favorite!");
     }
   }
 }
