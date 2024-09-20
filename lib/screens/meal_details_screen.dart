2757bd70f5ca905d77ab96b0f04c48d78fdde472
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-09-20 09:34:56 +0900

    Update

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -27,7 +27,16 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: animation,
+                  child: child,
+                );
+              },
+              duration: const Duration(milliseconds: 300),
+              child: Icon(isFavorite ? Icons.star : Icons.star_border),
+            ),
           ),
         ],
       ),
